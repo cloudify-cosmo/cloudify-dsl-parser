e787afe0de410364dda95fb7ab117283c88547c4
Itai Frenkel <itaif@gigaspaces.com>
2013-10-09 14:26:24 +0300

    Fixed project structure

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 cosmo-plugin-dsl-parser
-=======================
+===================
 
-DSL parser for cosmo
+This package contains a plugin that manipulates the recipe dsl
+
+- Build Status (develop master) [![Build Status](https://secure.travis-ci.org/CloudifySource/cosmo-plugin-dsl-parser.png?branch=master)](http://travis-ci.org/CloudifySource/cosmo-plugin-dsl-parser)
---
+++ setup.py
@@ -0,0 +1,22 @@
+__author__ = 'elip'
+
+from setuptools import setup
+
+COSMO_CELERY_VERSION = "0.1.1"
+COSMO_CELERY = "https://github.com/CloudifySource/cosmo-celery-common/tarball/{0}".format(COSMO_CELERY_VERSION)
+
+setup(
+    name='cosmo-plugin-dsl-parser',
+    version='0.1',
+    author='elip',
+    author_email='elip@gigaspaces.com',
+    packages=['plugin_installer'],
+    license='LICENSE',
+    description='Plugin for transforming recipe DSLs',
+    zip_safe=False,
+    install_requires=[
+        "cosmo-celery-common"
+    ],
+
+    dependency_links=["{0}#egg=cosmo-celery-common-{1}".format(COSMO_CELERY, COSMO_CELERY_VERSION)]
+)
