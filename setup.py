dc0af8f829b567940194d001dfbffaae5f886b10
Itai Frenkel <itaif@gigaspaces.com>
2013-10-14 15:13:55 +0300

    upgrade version to 0.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ COSMO_CELERY = "https://github.com/CloudifySource/cosmo-celery-common/tarball/{0
 
 setup(
     name='cosmo-plugin-dsl-parser',
-    version='0.1.1',
+    version='0.1.2',
     author='elip',
     author_email='elip@gigaspaces.com',
     packages=['dsl_parser'],
