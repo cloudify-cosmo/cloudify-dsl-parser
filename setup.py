fb4b3bb7b6a3e358378be1bac9327ea701a98ef2
Dan Kilman <dankilman@gmail.com>
2013-11-11 11:39:57 +0200

    fix plugin dependency version on develop branch

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ __author__ = 'elip'
 from setuptools import setup
 
 COSMO_CELERY_VERSION = "0.1.1"
-COSMO_CELERY_BRANCH = "master"
+COSMO_CELERY_BRANCH = "develop"
 COSMO_CELERY = "https://github.com/CloudifySource/cosmo-celery-common/tarball/{0}".format(COSMO_CELERY_BRANCH)
 
 setup(
