b107b64abb4433c92e925363cbcc3cded548ddf3
iliapolo <elip@gigaspaces.com>
2014-02-05 14:23:40 +0200

    CLOUDIFY-2370 remove cosmo-celery-common dependency

--- setup.py
+++ setup.py
@@ -2,14 +2,6 @@ __author__ = 'elip'
 
 from setuptools import setup
 
-COSMO_CELERY_VERSION = '0.3'
-COSMO_CELERY_BRANCH = 'feature/CLOUDIFY-2370-plugins-as-python-libs'
-COSMO_CELERY = "https://github.com/CloudifySource/" \
-               "cosmo-celery-common/tarball/{" \
-               "0}#egg=cosmo-celery-common-{1}" \
-    .format(COSMO_CELERY_BRANCH,
-            COSMO_CELERY_VERSION)
-
 setup(
     name='cosmo-plugin-dsl-parser',
     version='0.3',
@@ -20,10 +12,7 @@ setup(
     description='Plugin for transforming recipe DSLs',
     zip_safe=False,
     install_requires=[
-        "cosmo-celery-common",
         "PyYAML",
         'jsonschema'
-    ],
-
-    dependency_links=[COSMO_CELERY]
+    ]
 )
