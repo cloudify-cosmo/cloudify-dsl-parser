7b831ce45be3899129d157959fc4025b9183f122
Idan Moyal <idan@gigaspaces.com>
2014-04-02 12:01:19 +0300

    changed package name and version

--- setup.py
+++ setup.py
@@ -1,15 +1,30 @@
+########
+# Copyright (c) 2013 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    * See the License for the specific language governing permissions and
+#    * limitations under the License.
+
 __author__ = 'elip'
 
 from setuptools import setup
 
 setup(
-    name='cosmo-plugin-dsl-parser',
-    version='0.3',
+    name='cloudify-dsl-parser',
+    version='3.0',
     author='elip',
     author_email='elip@gigaspaces.com',
     packages=['dsl_parser'],
     license='LICENSE',
-    description='Plugin for transforming recipe DSLs',
+    description='Cloudify DSL parser',
     zip_safe=False,
     install_requires=[
         "PyYAML==3.10",
