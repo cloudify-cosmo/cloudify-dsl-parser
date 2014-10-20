7d51c07e1676cdb54f69cdd093abb7b240be6c0d
Dan Kilman <dankilman@gmail.com>
2014-10-20 15:03:41 +0300

    CFY-1433 add and improve tests

--- dsl_parser/tests/test_multi_instance.py
+++ dsl_parser/tests/test_multi_instance.py
@@ -511,15 +511,13 @@ node_templates:
         modification = self.modify_multi(plan, {
             'host': {'instances': 2}
         })
-        self._assert_modification(modification, 1, 0)
+        self._assert_modification(modification, 1, 0, 1, 0)
+        self._assert_added_not_in_previous(plan, modification)
         added_and_related = modification['added_and_related']
         for instance in added_and_related:
             self.assertEqual('host', instance['name'])
             self.assertIn('host_', instance['id'])
             self.assertEqual(instance['host_id'], instance['id'])
-        self._assert_added_not_in_previous(
-            previous_node_instances=plan['node_instances'],
-            added_and_related=added_and_related)
 
     def test_modified_single_node_removed(self):
         yaml = self.BASE_BLUEPRINT + """
@@ -532,7 +530,8 @@ node_templates:
         modification = self.modify_multi(plan, {
             'host': {'instances': 1}
         })
-        self._assert_modification(modification, 0, 1)
+        self._assert_modification(modification, 0, 1, 0, 1)
+        self._assert_removed_in_previous(plan, modification)
 
     def test_modified_single_node_added_with_child_contained_in_1(self):
         yaml = self.BASE_BLUEPRINT + """
@@ -548,7 +547,8 @@ node_templates:
         modification = self.modify_multi(plan, {
             'host': {'instances': 2}
         })
-        self._assert_modification(modification, 2, 0)
+        self._assert_modification(modification, 2, 0, 2, 0)
+        self._assert_added_not_in_previous(plan, modification)
         added_and_related = modification['added_and_related']
         host_nodes = self._nodes_by_name(added_and_related, 'host')
         self.assertEqual(1, len(host_nodes))
@@ -564,9 +564,33 @@ node_templates:
         self._assert_each_node_valid_hosted(db_nodes, host_nodes)
         self._assert_contained(self._nodes_relationships(db_nodes),
                                self._node_ids(host_nodes), 'host')
-        self._assert_added_not_in_previous(
-            previous_node_instances=plan['node_instances'],
-            added_and_related=added_and_related)
+
+    def test_modified_single_node_removed_with_child_contained_in_1(self):
+        yaml = self.BASE_BLUEPRINT + """
+    host:
+        type: cloudify.types.host
+        instances:
+            deploy: 2
+    db:
+        type: db
+        relationships:
+            -   type: cloudify.relationships.contained_in
+                target: host
+"""
+        plan = self.parse_multi(yaml)
+        modification = self.modify_multi(plan, {
+            'host': {'instances': 1}
+        })
+        self._assert_modification(modification, 0, 2, 0, 2)
+        self._assert_removed_in_previous(plan, modification)
+        removed_and_related = modification['removed_and_related']
+        host_nodes = self._nodes_by_name(removed_and_related, 'host')
+        self.assertEqual(1, len(host_nodes))
+        db_nodes = self._nodes_by_name(removed_and_related, 'db')
+        self.assertEqual(1, len(db_nodes))
+        self._assert_contained(self._nodes_relationships(db_nodes),
+                               self._node_ids(host_nodes), 'host')
+        self._assert_each_node_valid_hosted(db_nodes, host_nodes)
 
     def test_modified_single_node_added_with_child_contained_in_2(self):
         yaml = self.BASE_BLUEPRINT + """
@@ -582,7 +606,8 @@ node_templates:
         modification = self.modify_multi(plan, {
             'db': {'instances': 2}
         })
-        self._assert_modification(modification, 2, 0)
+        self._assert_modification(modification, 2, 0, 1, 0)
+        self._assert_added_not_in_previous(plan, modification)
         added_and_related = modification['added_and_related']
         host_nodes = self._nodes_by_name(added_and_related, 'host')
         self.assertEqual(1, len(host_nodes))
@@ -598,9 +623,33 @@ node_templates:
         self._assert_each_node_valid_hosted(db_nodes, host_nodes)
         self._assert_contained(self._nodes_relationships(db_nodes),
                                self._node_ids(host_nodes), 'host')
-        self._assert_added_not_in_previous(
-            previous_node_instances=plan['node_instances'],
-            added_and_related=added_and_related)
+
+    def test_modified_single_node_removed_with_child_contained_in_2(self):
+        yaml = self.BASE_BLUEPRINT + """
+    host:
+        type: cloudify.types.host
+    db:
+        type: db
+        instances:
+            deploy: 2
+        relationships:
+            -   type: cloudify.relationships.contained_in
+                target: host
+"""
+        plan = self.parse_multi(yaml)
+        modification = self.modify_multi(plan, {
+            'db': {'instances': 1}
+        })
+        self._assert_modification(modification, 0, 2, 0, 1)
+        self._assert_removed_in_previous(plan, modification)
+        removed_and_related = modification['removed_and_related']
+        host_nodes = self._nodes_by_name(removed_and_related, 'host')
+        self.assertEqual(1, len(host_nodes))
+        db_nodes = self._nodes_by_name(removed_and_related, 'db')
+        self.assertEqual(1, len(db_nodes))
+        self._assert_contained(self._nodes_relationships(db_nodes),
+                               self._node_ids(host_nodes), 'host')
+        self._assert_each_node_valid_hosted(db_nodes, host_nodes)
 
     def test_modified_two_nodes_added_with_child_contained_in(self):
         yaml = self.BASE_BLUEPRINT + """
@@ -617,7 +666,8 @@ node_templates:
             'host': {'instances': 2},
             'db': {'instances': 2}
         })
-        self._assert_modification(modification, 5, 0)
+        self._assert_modification(modification, 5, 0, 4, 0)
+        self._assert_added_not_in_previous(plan, modification)
         added_and_related = modification['added_and_related']
         host_nodes = self._nodes_by_name(added_and_related, 'host')
         self.assertEqual(2, len(host_nodes))
@@ -632,9 +682,35 @@ node_templates:
             self.assertIn('db_', instance['id'])
         self._assert_contained(self._nodes_relationships(db_nodes),
                                self._node_ids(host_nodes), 'host')
-        self._assert_added_not_in_previous(
-            previous_node_instances=plan['node_instances'],
-            added_and_related=added_and_related)
+
+    def test_modified_two_nodes_removed_with_child_contained_in(self):
+        yaml = self.BASE_BLUEPRINT + """
+    host:
+        type: cloudify.types.host
+        instances:
+            deploy: 2
+    db:
+        type: db
+        instances:
+            deploy: 2
+        relationships:
+            -   type: cloudify.relationships.contained_in
+                target: host
+"""
+        plan = self.parse_multi(yaml)
+        modification = self.modify_multi(plan, {
+            'host': {'instances': 1},
+            'db': {'instances': 1}
+        })
+        self._assert_modification(modification, 0, 5, 0, 4)
+        self._assert_removed_in_previous(plan, modification)
+        removed_and_related = modification['removed_and_related']
+        host_nodes = self._nodes_by_name(removed_and_related, 'host')
+        self.assertEqual(2, len(host_nodes))
+        db_nodes = self._nodes_by_name(removed_and_related, 'db')
+        self.assertEqual(3, len(db_nodes))
+        self._assert_contained(self._nodes_relationships(db_nodes),
+                               self._node_ids(host_nodes), 'host')
 
     def test_modified_single_node_added_with_connected_1(self):
         yaml = self.BASE_BLUEPRINT + """
@@ -654,7 +730,8 @@ node_templates:
         modification = self.modify_multi(plan, {
             'host1': {'instances': 2}
         })
-        self._assert_modification(modification, 3, 0)
+        self._assert_modification(modification, 3, 0, 2, 0)
+        self._assert_added_not_in_previous(plan, modification)
         added_and_related = modification['added_and_related']
         host1_nodes = self._nodes_by_name(added_and_related, 'host1')
         host2_nodes = self._nodes_by_name(added_and_related, 'host2')
@@ -675,9 +752,53 @@ node_templates:
                                self._node_ids(host1_nodes), 'host1')
         self._assert_all_to_all([node['relationships'] for node in db_nodes],
                                 self._node_ids(host2_nodes), 'host2')
-        self._assert_added_not_in_previous(
-            previous_node_instances=plan['node_instances'],
-            added_and_related=added_and_related)
+
+    def test_modified_single_node_removed_with_connected_1(self):
+        yaml = self.BASE_BLUEPRINT + """
+    host1:
+        type: cloudify.types.host
+        instances:
+            deploy: 2
+    host2:
+        type: cloudify.types.host
+        instances:
+            deploy: 2
+    db:
+        type: db
+        instances:
+            deploy: 2
+        relationships:
+            -   type: cloudify.relationships.contained_in
+                target: host1
+            -   type: cloudify.relationships.connected_to
+                target: host2
+"""
+        plan = self.parse_multi(yaml)
+        modification = self.modify_multi(plan, {
+            'host1': {'instances': 1}
+        })
+        self._assert_modification(modification, 0, 5, 0, 3)
+        self._assert_removed_in_previous(plan, modification)
+        removed_and_related = modification['removed_and_related']
+        host1_nodes = self._nodes_by_name(removed_and_related, 'host1')
+        host2_nodes = self._nodes_by_name(removed_and_related, 'host2')
+        self.assertEqual(1, len(host1_nodes))
+        self.assertEqual(2, len(host2_nodes))
+        for i, host_nodes in enumerate([host1_nodes, host2_nodes], start=1):
+            for instance in host_nodes:
+                self.assertEqual('host{0}'.format(i), instance['name'])
+                self.assertIn('host{0}_'.format(i), instance['id'])
+                self.assertEqual(instance['host_id'], instance['id'])
+        db_nodes = self._nodes_by_name(removed_and_related, 'db')
+        self.assertEqual(2, len(db_nodes))
+        for instance in db_nodes:
+            self.assertEqual('db', instance['name'])
+            self.assertIn('db_', instance['id'])
+        self._assert_each_node_valid_hosted(db_nodes, host1_nodes)
+        self._assert_contained(self._nodes_relationships(db_nodes),
+                               self._node_ids(host1_nodes), 'host1')
+        self._assert_all_to_all([node['relationships'] for node in db_nodes],
+                                self._node_ids(host2_nodes), 'host2')
 
     def test_modified_single_node_added_with_connected_2(self):
         yaml = self.BASE_BLUEPRINT + """
@@ -697,7 +818,8 @@ node_templates:
         modification = self.modify_multi(plan, {
             'host2': {'instances': 2}
         })
-        self._assert_modification(modification, 2, 0)
+        self._assert_modification(modification, 2, 0, 1, 0)
+        self._assert_added_not_in_previous(plan, modification)
         added_and_related = modification['added_and_related']
         host2_nodes = self._nodes_by_name(added_and_related, 'host2')
         self.assertEqual(1, len(host2_nodes))
@@ -712,9 +834,47 @@ node_templates:
             self.assertIn('db_', instance['id'])
         self._assert_all_to_all([node['relationships'] for node in db_nodes],
                                 self._node_ids(host2_nodes), 'host2')
-        self._assert_added_not_in_previous(
-            previous_node_instances=plan['node_instances'],
-            added_and_related=added_and_related)
+
+    def test_modified_single_node_removed_with_connected_2(self):
+        yaml = self.BASE_BLUEPRINT + """
+    host1:
+        type: cloudify.types.host
+        instances:
+            deploy: 2
+    host2:
+        type: cloudify.types.host
+        instances:
+            deploy: 2
+    db:
+        type: db
+        instances:
+            deploy: 2
+        relationships:
+            -   type: cloudify.relationships.contained_in
+                target: host1
+            -   type: cloudify.relationships.connected_to
+                target: host2
+"""
+        plan = self.parse_multi(yaml)
+        modification = self.modify_multi(plan, {
+            'host2': {'instances': 1}
+        })
+        self._assert_modification(modification, 0, 5, 0, 1)
+        self._assert_removed_in_previous(plan, modification)
+        removed_and_related = modification['removed_and_related']
+        host2_nodes = self._nodes_by_name(removed_and_related, 'host2')
+        self.assertEqual(1, len(host2_nodes))
+        for instance in host2_nodes:
+            self.assertEqual('host2', instance['name'])
+            self.assertIn('host2_', instance['id'])
+            self.assertEqual(instance['host_id'], instance['id'])
+        db_nodes = self._nodes_by_name(removed_and_related, 'db')
+        self.assertEqual(4, len(db_nodes))
+        for instance in db_nodes:
+            self.assertEqual('db', instance['name'])
+            self.assertIn('db_', instance['id'])
+        self._assert_all_to_all([node['relationships'] for node in db_nodes],
+                                self._node_ids(host2_nodes), 'host2')
 
     def test_modified_single_node_added_with_connected_3(self):
         yaml = self.BASE_BLUEPRINT + """
@@ -734,7 +894,8 @@ node_templates:
         modification = self.modify_multi(plan, {
             'db': {'instances': 2}
         })
-        self._assert_modification(modification, 3, 0)
+        self._assert_modification(modification, 3, 0, 1, 0)
+        self._assert_added_not_in_previous(plan, modification)
         added_and_related = modification['added_and_related']
         host1_nodes = self._nodes_by_name(added_and_related, 'host1')
         host2_nodes = self._nodes_by_name(added_and_related, 'host2')
@@ -755,9 +916,53 @@ node_templates:
                                self._node_ids(host1_nodes), 'host1')
         self._assert_all_to_all([node['relationships'] for node in db_nodes],
                                 self._node_ids(host2_nodes), 'host2')
-        self._assert_added_not_in_previous(
-            previous_node_instances=plan['node_instances'],
-            added_and_related=added_and_related)
+
+    def test_modified_single_node_removed_with_connected_3(self):
+        yaml = self.BASE_BLUEPRINT + """
+    host1:
+        type: cloudify.types.host
+        instances:
+            deploy: 2
+    host2:
+        type: cloudify.types.host
+        instances:
+            deploy: 2
+    db:
+        type: db
+        instances:
+            deploy: 2
+        relationships:
+            -   type: cloudify.relationships.contained_in
+                target: host1
+            -   type: cloudify.relationships.connected_to
+                target: host2
+"""
+        plan = self.parse_multi(yaml)
+        modification = self.modify_multi(plan, {
+            'db': {'instances': 1}
+        })
+        self._assert_modification(modification, 0, 6, 0, 2)
+        self._assert_removed_in_previous(plan, modification)
+        removed_and_related = modification['removed_and_related']
+        host1_nodes = self._nodes_by_name(removed_and_related, 'host1')
+        host2_nodes = self._nodes_by_name(removed_and_related, 'host2')
+        self.assertEqual(2, len(host1_nodes))
+        self.assertEqual(2, len(host2_nodes))
+        for i, host_nodes in enumerate([host1_nodes, host2_nodes], start=1):
+            for instance in host_nodes:
+                self.assertEqual('host{0}'.format(i), instance['name'])
+                self.assertIn('host{0}_'.format(i), instance['id'])
+                self.assertEqual(instance['host_id'], instance['id'])
+        db_nodes = self._nodes_by_name(removed_and_related, 'db')
+        self.assertEqual(2, len(db_nodes))
+        for instance in db_nodes:
+            self.assertEqual('db', instance['name'])
+            self.assertIn('db_', instance['id'])
+        self._assert_each_node_valid_hosted(db_nodes, host1_nodes)
+        self._assert_contained(self._nodes_relationships(db_nodes),
+                               self._node_ids(host1_nodes), 'host1')
+        self._assert_all_to_all([node['relationships'] for node in db_nodes],
+                                self._node_ids(host2_nodes), 'host2')
 
     def _nodes_relationships(self, nodes, target_name=None):
         relationships = []
@@ -768,9 +973,9 @@ node_templates:
                 relationships.append(rel)
         return relationships
 
-    def _assert_added_not_in_previous(self,
-                                      previous_node_instances,
-                                      added_and_related):
+    def _assert_added_not_in_previous(self, plan, modification):
+        previous_node_instances = plan['node_instances']
+        added_and_related = modification['added_and_related']
         previous_graph = rel_graph.build_node_graph(previous_node_instances)
         added_nodes_graph = rel_graph.build_node_graph(added_and_related)
         for instance_id, data in added_nodes_graph.nodes_iter(data=True):
@@ -782,11 +987,34 @@ node_templates:
         for source, target, in added_nodes_graph.edges_iter():
             self.assertFalse(previous_graph.has_edge(source, target))
 
+    def _assert_removed_in_previous(self, plan, modification):
+        previous_node_instances = plan['node_instances']
+        removed_and_related = modification['removed_and_related']
+        previous_graph = rel_graph.build_node_graph(previous_node_instances)
+        removed_nodes_graph = rel_graph.build_node_graph(removed_and_related)
+        for instance_id, data in removed_nodes_graph.nodes_iter(data=True):
+            self.assertIn(instance_id, previous_graph)
+        for source, target, in removed_nodes_graph.edges_iter():
+            self.assertTrue(previous_graph.has_edge(source, target))
+
     def _assert_modification(self,
                              modification,
-                             expected_workflow_added_count,
-                             expected_workflow_removed_count):
-        self.assertEqual(expected_workflow_added_count,
-                         len(modification['added_and_related']))
-        self.assertEqual(expected_workflow_removed_count,
-                         len(modification['removed_and_related']))
+                             expected_added_and_related_count,
+                             expected_removed_and_related_count,
+                             expected_added_count,
+                             expected_removed_count):
+        added_and_related = modification['added_and_related']
+        removed_and_related = modification['removed_and_related']
+        added = [instance for instance in added_and_related
+                 if instance.get('modification') == 'added']
+        removed = [instance for instance in removed_and_related
+                   if instance.get('modification') == 'removed']
+
+        self.assertEqual(expected_added_and_related_count,
+                         len(added_and_related))
+        self.assertEqual(expected_removed_and_related_count,
+                         len(removed_and_related))
+        self.assertEqual(expected_added_count,
+                         len(added))
+        self.assertEqual(expected_removed_count,
+                         len(removed))
