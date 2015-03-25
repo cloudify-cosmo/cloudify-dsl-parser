2565bc0dbc5880560bd3b89bd7db3568e8888393
opencm <limor@gigaspaces.com>
2015-03-25 12:03:42 +0200

    Bump version to 3.2m8

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ except ImportError, e:
 
 setup(
     name='cloudify-dsl-parser',
-    version='3.2a7',
+    version='3.2a8',
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
     packages=['dsl_parser',
