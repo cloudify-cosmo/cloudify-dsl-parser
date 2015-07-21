9c3c5b152c7d1abe364ffbe7effe5169d73212e9
yaelpeled <yael@gigaspaces.com>
2015-07-21 16:34:51 +0300

    CFY-3000 flake8

--- dsl_parser/elements/imports.py
+++ dsl_parser/elements/imports.py
@@ -12,11 +12,9 @@
 #    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
-import contextlib
 
 import os
 import urllib
-import urllib2
 
 import networkx as nx
 
