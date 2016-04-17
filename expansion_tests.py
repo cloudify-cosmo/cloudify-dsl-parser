ebe8a7295db91ecc377f8b10f0ad659b93ce3bb5
liormizr <liorm@gigaspaces.com>
2016-04-17 15:30:55 +0300

    CFY-5143 sort expansion demo

--- expansion_tests.py
+++ expansion_tests.py
@@ -4,6 +4,7 @@
 from __future__ import print_function
 
 import sys
+import pprint
 from functools import wraps, partial
 
 from tosca_parser.framework.elements.blueprint import Blueprint
@@ -60,20 +61,52 @@ def main_demo():
     print('Check element replacement expanding:')
     print("    for this test we'll run the parser")
     print('tosca-template: {0}'.format(tosca_template))
-    parse(tosca_template)
+    plane = parse(tosca_template)
+
+    print('results:')
+    print('- plane keys: {0}'.format(plane.keys()))
+    print('- plane version: {0}'.format(plane.version))
+    print('- plane outputs: {0}'.format(plane.outputs.keys()))
+    # print('- plane node_template:')
+    # pprint.pprint(plane.node_templates)
+    # print('- plane inputs:')
+    # pprint.pprint(plane.inputs)
     print('Success!...')
 
 
-def print_func_args(func=None, prefix='DEBUG:'):
+def clean_main_demo():
+    TestBlueprint = print_class_methods_args(
+        Blueprint, prefix='Aria.parser Expansion Demo:\n')
+    element_extension = ElementExtension(
+        action=ElementExtension.REPLACE_ELEMENT_ACTION,
+        target_element=Blueprint,
+        new_element=TestBlueprint)
+
+    function_expantion = IntrinsicFunctionExtension(
+        action=IntrinsicFunctionExtension.ADD_FUNCTION_ACTION,
+        name='test_function',
+        function=type('TestFunction', (Function,), {}))
+
+    extend(element_extentions=[element_extension],
+           function_expansions=[function_expantion])
+
+    tosca_template = (
+        '/home/liorm/work/workspace/bootstrap/cloudify-manager-blueprints/'
+        'openstack-manager-blueprint.yaml')
+    parse(tosca_template)
+
+
+def print_func_args(func=None, prefix='DEBUG:', class_name=None):
     """
     Decorator to print function call details - parameters names and effective values
     !!!Do not use on production ONLY for debug!!!
     :param func: decorated function to analyze
     :param prefix: prifix for the debug prints
+    :param class_name: add class name to func' path
     :return: print_func_args wrapper
     """
     if func is None:
-        return partial(print_func_args, prefix=prefix)
+        return partial(print_func_args, prefix=prefix, class_name=class_name)
 
     @wraps(func)
     def wrapper(*func_args, **func_kwargs):
@@ -88,9 +121,9 @@ def print_func_args(func=None, prefix='DEBUG:'):
             params.append(('args', args))
         if func_kwargs:
             params.append(('kwargs', func_kwargs))
-        try:
-            func_path = func.__qualname__
-        except AttributeError:
+        if class_name:
+            func_path = func.__module__ + ':' + class_name + '.' + func.__name__
+        else:
             func_path = func.__module__ + '.' + func.__name__
         print('%s %s(%s)' % (prefix, func_path, ', '.join('%s = %r' % p for p in params)))
         return func(*func_args, **func_kwargs)
@@ -111,8 +144,10 @@ def print_class_methods_args(cls=None, prefix='DEBUG:'):
 
     for name, method in vars(cls).iteritems():
         if callable(method):
-            setattr(cls, name, print_func_args(method, prefix))
-    cls.__init__ = print_func_args(cls.__init__, prefix)
+            setattr(cls, name, print_func_args(
+                method, prefix, class_name=cls.__name__))
+    cls.__init__ = print_func_args(
+        cls.__init__, prefix, class_name=cls.__name__)
     return cls
 
 if __name__ == '__main__':
